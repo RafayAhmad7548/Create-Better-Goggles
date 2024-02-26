a29921d76f677e99aa0847836d5732b36d69e0c6
RafayAhmad7548 <rafayahmad1425@gmail.com>
2024-02-26 10:09:20 +0500

    Started Project

---
+++ src/main/java/com/rmods/bettergoggles/items/ModItems.java
@@ -0,0 +1,28 @@
+package com.rmods.bettergoggles.items;
+
+import com.rmods.bettergoggles.BetterGoggles;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems{
+
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, BetterGoggles.MODID);
+
+    public static final RegistryObject<Item> BETTER_GOGGLE = 
+        ITEMS.register("better_goggle", () -> new BetterGoggle(new Item.Properties().stacksTo(1)));
+
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+
+
+
+
+
+
+}
