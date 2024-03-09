fba714cce852526569b5efebe87db7aa2f155017
RafayAhmad7548 <rafayahmad1425@gmail.com>
2024-03-09 08:15:35 +0500

    Added recipes for fluid and goggles

--- src/main/java/com/rmods/bettergoggles/fluids/ModFluids.java
+++ src/main/java/com/rmods/bettergoggles/fluids/ModFluids.java
@@ -9,16 +9,22 @@ import static com.rmods.bettergoggles.BetterGoggles.REGISTRATE;
 public class ModFluids{
 
     public static final FluidEntry<ForgeFlowingFluid.Flowing> MOLTEN_DIAMOND =
-        REGISTRATE.fluid("molten_diamond", new ResourceLocation("bettergoggles:fluid/molten_diamond_still"), new ResourceLocation("bettergoggles:fluid/molten_diamond_flow"))
+        REGISTRATE.fluid("molten_diamond", new ResourceLocation("bettergoggles:block/molten_diamond_still"), new ResourceLocation("bettergoggles:block/molten_diamond_flow"))
             .lang("Molten Diamond")
-            .properties(b -> b.canPushEntity(true).density(500).viscosity(3000))
-            .fluidProperties(b -> b.levelDecreasePerBlock(2))
+            .properties(b -> b.canPushEntity(true).density(1000).viscosity(3000))
+            .fluidProperties(b -> b.levelDecreasePerBlock(3).tickRate(4))
+            .register();
+
+
+    public static final FluidEntry<ForgeFlowingFluid.Flowing> MOLTEN_NETHERITE =
+        REGISTRATE.fluid("molten_netherite", new ResourceLocation("bettergoggles:block/molten_netherite_still"), new ResourceLocation("bettergoggles:block/molten_netherite_flow"))
+            .lang("Molten Netherite")
+            .properties(b -> b.canPushEntity(true).density(1000).viscosity(3000))
+            .fluidProperties(b -> b.levelDecreasePerBlock(3).tickRate(4))
             .register();
 
     public static void register(){
         
     }
-        
-
 
 }
